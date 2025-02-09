a744afa9dfc9d6ba78820f03d70cb81aa48003c3
Samuel Bruce <samuelbruce99@gmail.com>
2025-02-09 21:42:27 +0000

    adding scale and float wrap

--- plt.c
+++ plt.c
@@ -1,3 +1,3 @@
 #include "plt.h"
 
-K draw(K x,K y){FILE *g=popen("gnuplot -persistent 2>/dev/null","w");GS("xl 'X'");GS("yl 'Y'");G("pl '-' w l");DO(xn,G("%g %g",kF(x)[i],kF(y)[i]));G("e");pclose(g);(K)0;};
+K draw(K x,K y){FILE *g=popen("gnuplot -persistent 2>/dev/null","w");GS("autoscale");G("pl '-' w l");DO(xn,G("%g %g",kF(x)[i],kF(y)[i]));G("e");pclose(g);(K)0;};
--- plt.q
+++ plt.q
@@ -1,1 +1,2 @@
-plt:`plt 2:(`draw;2);
+.plt.util.plt:`plt 2:(`draw;2);
+.plt.plt:{.plt.util.plt[`float$x;`float$y]};
