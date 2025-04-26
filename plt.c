0ca37d976f345fdb4fb7e5121a8d8e5c71a790e3
Samuel Bruce <samuelbruce99@gmail.com>
2025-04-26 15:18:53 +0100

    Condensed matrix

--- plt.c
+++ plt.c
@@ -1,27 +1,13 @@
 #include "plt.h"
 
-K draw(K x,K y){FILE *g=popen("gnuplot -persistent 2>/dev/null","w");GS("autoscale");G("pl '-' w l");DO(xn,G("%g %g",kF(x)[i],kF(y)[i]));G("e");pclose(g);(K)0;};
+K draw(K x,K y){FP g=GP();GS("autoscale");GN("pl '-' w l");DO(xn,GN("%g %g",kF(x)[i],kF(y)[i]));GN("e");pclose(g);(K)0;};
 
 K heatmap(K x) {
-    FILE *g = popen("gnuplot -persistent 2>/dev/null", "w");
-    int rows = x->n;
-    int cols = kK(x)[0]->n;
-    
-    GS("palette defined (0 'blue', 0.5 'white', 1 'red')");
-    GS("view map");
-    GS("autoscale");
-    G("set pm3d map");
-    G("splot '-' matrix with pm3d");
-    
-    for(int i = 0; i < rows; i++) {
-        K row = kK(x)[i];
-        for(int j = 0; j < cols; j++) {
-            fprintf(g, "%g ", kF(row)[j]);
-        }
-        fprintf(g, "\n");
-    }
-    fprintf(g, "e\n");
-
-    pclose(g);
-    return (K)0;
+    FP g=GP();
+    I rn = x->n;
+    I cn = kK(x)[0]->n;
+    GS("palette defined (0 'blue', 0.5 'white', 1 'red')");GS("view map");GS("autoscale");GS("pm3d map");
+    GN("splot '-' matrix with pm3d");
+    DO(rn, K r = kK(x)[i]; DOJ(cn, G("%g ", kF(r)[j]));GN(""));GN("e");
+    pclose(g);(K)0;
 }
--- plt.h
+++ plt.h
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include "k.h"
 
-#define G(s,...) fprintf(g,s"\n",##__VA_ARGS__)
-#define GS(s) G("set "s)
+#define DOJ(n,x)  {J j=0,_j=(n);for(;j<_j;++j){x;}}
+
+#define G(s,...) fprintf(g,s,##__VA_ARGS__)
+#define GN(s,...) fprintf(g,s"\n",##__VA_ARGS__)
+#define GS(s) GN("set "s)
+
 typedef FILE* FP;
 FP GP() {R popen("gnuplot -persistent 2>/dev/null", "w");}
